ed9fca93db3232a7e5485f02d29d311fc14a7673
zikriaulia28 <zikriaulia98@gmail.com>
2023-02-28 10:34:43 +0700

    update profile and history

--- Profile/main.js
+++ Profile/main.js
@@ -1,6 +1,9 @@
 const humberger = document.getElementById('humberger');
 const menuList = document.getElementById('menu-list');
 const profileList = document.getElementById('menu-profile');
+const emailInput = document.getElementById("email");
+const numberInput = document.getElementById("number");
+const addressInput = document.getElementById("address");
 
 humberger.addEventListener('click', function (e) {
   e.stopPropagation();
@@ -15,39 +18,38 @@ window.addEventListener('click', function () {
 
 function enableEdit() {
   // Hapus atribut "readonly" pada elemen input
-  document.getElementById("email").removeAttribute("readonly");
-  document.getElementById("number").removeAttribute("readonly");
-  document.getElementById("address").removeAttribute("readonly");
+  emailInput.removeAttribute("readonly");
+  numberInput.removeAttribute("readonly");
+  addressInput.removeAttribute("readonly");
 }
 
 function disableEdit() {
   // Tambahkan atribut "readonly" pada elemen input
-  document.getElementById("email").setAttribute("readonly", "");
-  document.getElementById("number").setAttribute("readonly", "");
-  document.getElementById("address").setAttribute("readonly", "");
+  emailInput.setAttribute("readonly", "");
+  numberInput.setAttribute("readonly", "");
+  addressInput.setAttribute("readonly", "");
 }
 
-
 function saveForm() {
-  const emailValue = document.getElementById("email").value;
-  const numberValue = document.getElementById("number").value;
-  const addressValue = document.getElementById("address").value;
-
   // Simpan perubahan pada localStorage
-  localStorage.setItem("email", emailValue);
-  localStorage.setItem("number", numberValue);
-  localStorage.setItem("address", addressValue);
+  localStorage.setItem("email", emailInput.value);
+  localStorage.setItem("number", numberInput.value);
+  localStorage.setItem("address", addressInput.value);
 
   disableEdit();
 }
 
 function startForm() {
   // Periksa apakah ada nilai yang disimpan di localStorage
-  if (localStorage.getItem("email") !== null) {
+  const emailValue = localStorage.getItem("email");
+  const numberValue = localStorage.getItem("number");
+  const addressValue = localStorage.getItem("address");
+
+  if (emailValue !== null) {
     // Jika ada, isi nilai-nilai input dari localStorage
-    document.getElementById("email").value = localStorage.getItem("email");
-    document.getElementById("number").value = localStorage.getItem("number");
-    document.getElementById("address").value = localStorage.getItem("address");
+    emailInput.value = emailValue;
+    numberInput.value = numberValue;
+    addressInput.value = addressValue;
   }
 }
 
